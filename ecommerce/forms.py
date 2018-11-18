96dc8312545520d3757b02beefe94a753f6f5f89
Toti Cavalcanti <toticavalcanti@hotmail.com>
2018-11-18 19:27:57 -0200

    put widget forms.PasswordInput in LoginForm class in ecommerce\forms.py

--- ecommerce/forms.py
+++ ecommerce/forms.py
@@ -34,4 +34,4 @@ class ContactForm(forms.Form):
 
 class LoginForm(forms.Form):
     username = forms.CharField()
-    password = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
