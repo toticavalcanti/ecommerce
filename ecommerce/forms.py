8b33d000646f71b356a49519a5f774efad8bf78f
Toti Cavalcanti <toticavalcanti@hotmail.com>
2018-11-18 15:29:09 -0200

    alter ident in ecommerce\forms.py

--- ecommerce/forms.py
+++ ecommerce/forms.py
@@ -1,6 +1,12 @@
 from django import forms
 
 class ContactForm(forms.Form):
-    full_name = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control", "placeholder": "Your full name"}))
+    full_name = forms.CharField(widget=forms.TextInput(
+                attrs={
+                    "class": "form-control", 
+                    "placeholder": "Your full name"
+                    }
+                )
+            )
     email     = forms.EmailField()
     content   = forms.CharField()
