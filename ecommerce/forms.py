de04fe4d6cba02c6635859b67adc7d78deca6e89
Toti Cavalcanti <toticavalcanti@hotmail.com>
2018-11-18 15:59:19 -0200

    define the clean_email method in ecommerce\forms.py

--- ecommerce/forms.py
+++ ecommerce/forms.py
@@ -25,3 +25,9 @@ class ContactForm(forms.Form):
                 }
             )
         )
+    
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        if not "gmail.com" in email:
+            raise forms.ValidationError("Email has to be gmail.com")
+        return email
--- ecommerce/views.py
+++ ecommerce/views.py
@@ -18,11 +18,13 @@ def about_page(request):
 	return render(request, "about/view.html", context)
 
 def contact_page(request):
-	contact_form = ContactForm()
+	contact_form = ContactForm(request.POST or None)
 	context = {
 				"title": "Contact Page",
 				"content": "Bem vindo a Contact Page",
-				"form": contact_form
+				"form": contact_form	
 			  }
+	if contact_form.is_valid():
+		print(contact_form.cleaned_data)
 	return render(request, "contact/view.html", context)
 
