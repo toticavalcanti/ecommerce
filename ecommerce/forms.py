0096d87cbd2ff46893d89107babd2a40fe6878e4
Toti Cavalcanti <toticavalcanti@hotmail.com>
2018-11-18 15:24:48 -0200

    put widget TextInput and named attributes in field full_name in ecommerce\forms.py

--- ecommerce/forms.py
+++ ecommerce/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
 class ContactForm(forms.Form):
-    full_name = forms.CharField()
+    full_name = forms.CharField(widget=forms.TextInput(attr={"class": "form-control", "id": "form_full_name"}))
     email     = forms.EmailField()
     content   = forms.CharField()
