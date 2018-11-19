b604e270db94ee1464a05b091be65b446a98b3f1
toti.cavalcanti <toticavalcanti@hotmail.com>
2018-11-19 21:13:57 -0200

    set the clean method in RegisterForm class in ecommerce/forms.py

--- ecommerce/forms.py
+++ ecommerce/forms.py
@@ -41,3 +41,10 @@ class RegisterForm(forms.Form):
     email = forms.EmailField()
     password = forms.CharField(widget=forms.PasswordInput)
     password2 = forms.CharField(label='Confirm password', widget=forms.PasswordInput)
+
+    def clean(self):
+        data = self.cleaned_data
+        password = self.cleaned_data.get('password')
+        password2 = self.cleaned_data.get('password2')
+        if password != password2:
+            raise forms.ValidationError("As senhas informadas devem ser iguais!")
