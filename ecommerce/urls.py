23e425b43f05f6ba644fa1a47e52d30148732dc4
toti.cavalcanti <toticavalcanti@hotmail.com>
2018-11-19 21:06:41 -0200

    add register_page in ecommerce/urls.py

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -16,12 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from .views import home_page, about_page, contact_page, login_page
+from .views import home_page, about_page, contact_page, login_page, register_page
 
 urlpatterns = [
 	path('', home_page),
 	path('about/', about_page),
 	path('contact/', contact_page),
     path('login/', login_page),
+    path('register/', register_page),
 	path('admin/', admin.site.urls),
 ]
