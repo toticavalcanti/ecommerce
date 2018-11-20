215429fd182be7528eee95a3c354670a6b96017e
toti.cavalcanti <toticavalcanti@hotmail.com>
2018-11-20 14:21:23 -0200

    configuration of static files in settings.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -118,3 +118,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static_my_project"),
+]
+
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn")
--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -13,6 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
+
 from django.contrib import admin
 from django.urls import path
 
@@ -26,3 +29,6 @@ urlpatterns = [
     path('register/', register_page),
 	path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
