91f066662a8638dd37acb15ea53fc23a79e82c26
toticavalcanti <toticavalcanti@hotmail.com>
2018-11-24 22:40:44 -0200

    create ProductListView class and product_list_view method in products/views.py and put in urls.py

--- products/views.py
+++ products/views.py
@@ -1,3 +1,14 @@
+from django.views.generic import ListView
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Product
+
+class ProductListView(ListView):
+    queryset = Product.objects.all()
+    template_name = "products/list.html"
+
+def product_list_view(request):
+    context = {
+        'qs': queryset
+    }
+    return render(request, "products/list.html", context)
