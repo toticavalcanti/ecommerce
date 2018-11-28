4091bdb603ededac76cf0c0b51e8471e7bcebf56
toti.cavalcanti <toticavalcanti@hotmail.com>
2018-11-28 11:14:55 -0200

    set src/products/templates/products/list.html

--- products/views.py
+++ products/views.py
@@ -7,8 +7,14 @@ class ProductListView(ListView):
     queryset = Product.objects.all()
     template_name = "products/list.html"
 
+    # def get_context_data(self, *args, **kwargs):
+    #     context = super(ProductListView, self).get_context_data(*args, **kwargs)
+    #     print(context)
+    #     return context
+
+
 def product_list_view(request):
     context = {
-        'qs': queryset
+        'object_list': queryset
     }
     return render(request, "products/list.html", context)
