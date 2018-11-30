b0308a7e5f58d996162c141cc46f03aea9794be4
toticavalcanti <toticavalcanti@hotmail.com>
2018-11-30 15:39:01 -0200

    Insert image in products/templates/products/detail.html and update product_detail_view() method to raise Http404 in products/views.py

--- products/views.py
+++ products/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from django.views.generic import ListView, DetailView
 from django.shortcuts import render, get_object_or_404
 
@@ -33,7 +34,12 @@ class ProductDetailView(DetailView):
 
 def product_detail_view(request, pk = None, *args, **kwargs):
     #instance = Product.objects.get(pk = pk) #id
-    instance = get_object_or_404(Product, pk = pk)
+    #instance = get_object_or_404(Product, pk = pk)
+    qs = Product.objects.filter(id = pk)
+    if qs.exists() and qs.count() == 1:
+        instance = qs.first()
+    else:
+        raise Http404("Product does not exist!")
     context = {
         'object': instance
     }
