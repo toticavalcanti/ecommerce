b0fe4e906bf64e39cc840d889356ca09372c7ffa
toticavalcanti <toticavalcanti@hotmail.com>
2018-11-30 14:25:12 -0200

    create and set upload_image_path and get_filename_ext in products/models.py

--- products/models.py
+++ products/models.py
@@ -1,10 +1,32 @@
+import random
+import os
 from django.db import models
+
+def get_filename_ext(filepath):
+    base_name = os.path.basename(filepath)
+    name, ext = os.path.splitext(base_name)
+    return name, ext
+
+
+def upload_image_path(instance, filename):
+    new_filename = random.randint(1, 3910209312)
+    name, ext = get_filename_ext(filename)
+    final_filename = '{new_filename}{ext}'.format(new_filename = new_filename, ext = ext) #old format
+    #final_filename = f"{new_filename}{ext}" #syntax to python 3.6 and up
+
+    return "products/{new_filename}/{final_filename}".format(
+                                                              new_filename = new_filename, 
+                                                              final_filename = final_filename
+                                                            )
+    #return f"products/{new_filename}/{new_filename}" #syntax to python 3.6 and up
 
 # Create your models here.
 class Product(models.Model): #product_category
     title       = models.CharField(max_length=120)
     description = models.TextField()
     price       = models.DecimalField(decimal_places=2, max_digits=20, default=39.99)
+    image       = models.FileField(upload_to = 'products/', null = True, blank = True)
+
 
     def __str__(self):
         return self.title
