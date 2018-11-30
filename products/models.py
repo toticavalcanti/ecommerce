e64053916bfe53c141665713c630d6a90e209fd1
toticavalcanti <toticavalcanti@hotmail.com>
2018-11-30 14:58:25 -0200

    create and set upload_image_path and get_filename_ext in products/models.py

--- products/models.py
+++ products/models.py
@@ -15,17 +15,18 @@ def upload_image_path(instance, filename):
     #final_filename = f"{new_filename}{ext}" #syntax to python 3.6 and up
 
     return "products/{new_filename}/{final_filename}".format(
-                                                              new_filename = new_filename, 
-                                                              final_filename = final_filename
-                                                            )
+            new_filename = new_filename, 
+            final_filename = final_filename
+            )
     #return f"products/{new_filename}/{new_filename}" #syntax to python 3.6 and up
+
 
 # Create your models here.
 class Product(models.Model): #product_category
     title       = models.CharField(max_length=120)
     description = models.TextField()
     price       = models.DecimalField(decimal_places=2, max_digits=20, default=39.99)
-    image       = models.FileField(upload_to = 'products/', null = True, blank = True)
+    image       = models.ImageField(upload_to = upload_image_path, null = True, blank = True)
 
 
     def __str__(self):
