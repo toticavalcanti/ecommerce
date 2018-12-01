649ac880ef7c264b7e72fba44bf74bcc4959d1b4
toticavalcanti <toticavalcanti@hotmail.com>
2018-11-30 22:08:39 -0200

    fix line 43 in url.py, ProductFeaturedListView to ProductFeaturedDetailView

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -40,11 +40,11 @@ urlpatterns = [
     path('login/', login_page),
     path('register/', register_page),
     path('featured/', ProductFeaturedListView.as_view()),
-    path('featured/<int:pk>', ProductFeaturedListView.as_view()),
+    path('featured/<int:pk>/', ProductFeaturedDetailView.as_view()),
     path('products/', ProductListView.as_view()),
     path('products-fbv/', product_list_view),
-    path('products/<int:pk>', ProductDetailView.as_view()),
-    path('products-fbv/<int:pk>', product_detail_view),
+    path('products/<int:pk>/', ProductDetailView.as_view()),
+    path('products-fbv/<int:pk>/', product_detail_view),
 	path('admin/', admin.site.urls),
 ]
 
